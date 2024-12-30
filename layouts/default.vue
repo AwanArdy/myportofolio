5e69453f7766ad57cb1e7f756e59e273af84d0a5
AwanArdy <muhammadawan46@gmail.com>
2024-12-30 22:33:51 +0800

    update

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,9 +4,11 @@
         <main class="pt-16">
             <slot />
         </main>
+        <SpeedInsights/>
     </div>
 </template>
 
 <script setup>
 import Navbar from '~/components/Navbar.vue';
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
 </script>
